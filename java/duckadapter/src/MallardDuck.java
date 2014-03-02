ff9d9bfb61e1d03b649ca19aaa32097205e0fea7
lyyyuna <lyyyuna@gmail.com>
2014-03-02 09:24:35 +0800

    state

---
+++ java/duckadapter/src/MallardDuck.java
@@ -0,0 +1,11 @@
+
+public class MallardDuck implements Duck {
+
+	public void quack() {
+		System.out.println("quack");
+	}
+	
+	public void fly() {
+		System.out.println("i am flying");
+	}
+}
---
+++ java/duckadapter/src/TestMain.java
@@ -0,0 +1,25 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		MallardDuck duck = new MallardDuck();
+		
+		WildTurkey turkey = new WildTurkey();
+		Duck turkeyAdapter = new TurkeyAdapter(turkey);
+		
+		System.out.println("the turkey says ...");
+		turkey.gobble();
+		turkey.fly();
+		
+		System.out.println("the duck says ...");
+		testDuck(duck);
+		
+		System.out.println("the turkeyadapter says");
+		testDuck(turkeyAdapter);
+	}
+	
+	static void testDuck(Duck duck) {
+		duck.quack();
+		duck.fly();
+	}
+}
