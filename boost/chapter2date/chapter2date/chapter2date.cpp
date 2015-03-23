4d4dbf6efb1ba2e97c1f7616cfaff9086c5dbe4c
lyyyuna <lyyyuna@gmail.com>
2015-03-23 13:50:00 +0800

    regex

--- boost/chapter2date/chapter2date/chapter2date.cpp
+++ boost/chapter2date/chapter2date/chapter2date.cpp
@@ -8,6 +8,8 @@
 #include <boost\enable_shared_from_this.hpp>
 #include <boost\make_shared.hpp>
 
+#include <boost\xpressive\xpressive_dynamic.hpp>
+#include <boost\assert.hpp>
 using namespace boost;
 
 class self_shared : public boost::enable_shared_from_this < self_shared >
@@ -27,14 +29,16 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << "min elapsed " << t.elapsed_min() << "s" << endl;
 	cout << "max elapsed " << t.elapsed_max() / 3600 <<  "h" << endl;
 
-	auto sp = make_shared<self_shared>(314);
+	auto sp = boost::make_shared<self_shared>(314);
 	sp->print();
 	auto p = sp->shared_from_this();
 	p->x = 1002;
 	p->print();
 	sp->print();
-
-
+	//using namespace boost::xpressive;
+	//cregex reg = cregex::compile("a.c");
+	//assert(regex_match("abc", reg));
+	assert(1);
 	cout << "time elapsed " << t.elapsed() << endl;
 
 
