ad17719584bc37e166a6dfac7959b0e698967da0
lyyyuna <lyyyuna@gmail.com>
2015-12-06 20:58:49 +0800

    adddevice finished

--- driver/PRamdisk/PRamdisk/pramdisk.c
+++ driver/PRamdisk/PRamdisk/pramdisk.c
@@ -32,6 +32,12 @@ NTSTATUS RamdiskEvtDeviceAdd(IN WDFDRIVER Drvier, IN PWDFDEVICE_INIT DeviceInit)
 	WDFDEVICE device;
 
 	PDEVICE_EXTENSION pDeviceExtension;
+	
+	WDF_IO_QUEUE_CONFIG ioQueueConfig;
+	WDF_OBJECT_ATTRIBUTES queueAttributes;
+	WDFQUEUE queue;
+	PQUEUE_EXTENSION pQueueExtension;
+
 
 	DECLARE_CONST_UNICODE_STRING(ntDeviceName, NT_DEVICE_NAME);
 
@@ -60,4 +66,128 @@ NTSTATUS RamdiskEvtDeviceAdd(IN WDFDRIVER Drvier, IN PWDFDEVICE_INIT DeviceInit)
 	}
 	// get device extension
 	pDeviceExtension = DeviceGetExtension(device);
+
+	// init the queue
+	WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&ioQueueConfig, WdfIoQueueDispatchSequential);
+
+	ioQueueConfig.EvtIoDeviceControl = RamdiskEvtIoDeviceControl;
+	ioQueueConfig.EvtIoRead = RamdiskEvtIoRead;
+	ioQueueConfig.EvtIoWrite = RamdiskEvtIoWrite;
+
+	WDF_OBJECT_ATTRIBUTES_INIT_CONTEXT_TYPE(&queueAttributes, QUEUE_EXTENSION);
+
+	status = WdfIoQueueCreate(device, &ioQueueConfig, &queueAttributes, &queue);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("queue create failed!!! \r\n"));
+		return status;
+	}
+	pQueueExtension = QueueGetExtension(queue);
+	pQueueExtension->DeviceExtension = pDeviceExtension;
+
+	pDeviceExtension->DiskRegInfo.DriverLetter.Buffer = (PWSTR)&pDeviceExtension->DriverLetterBuffer;
+	pDeviceExtension->DiskRegInfo.DriverLetter.MaximumLength = sizeof(pDeviceExtension->DriverLetterBuffer);
+
+	// retrieve info from registry
+	RamdiskQueryDiskRegParameters(WdfDriverGetRegistryPath(WdfDeviceGetDriver(device)), &pDeviceExtension->DiskRegInfo);
+
+	// alloc memory
+	pDeviceExtension->DiskImage = ExAllocatePoolWithTag(NonPagedPool, pDeviceExtension->DiskRegInfo.DiskSize, RAMDISK_TAG);
+	// if success
+	if (pDeviceExtension->DiskImage)
+	{
+		UNICODE_STRING win32Name;
+
+		RamdiskFormatDisk(pDeviceExtension);
+
+		// setup symbolic
+		RtlInitUnicodeString(&win32Name, DOS_DEVICE_NAME);
+		pDeviceExtension->SymbolicLink.Buffer = (PWSTR)&pDeviceExtension->DosDeviceNameBuffer;
+		pDeviceExtension->SymbolicLink.MaximumLength = sizeof(pDeviceExtension->DosDeviceNameBuffer);
+		pDeviceExtension->SymbolicLink.Length = win32Name.Length;
+
+		RtlCopyUnicodeString(&pDeviceExtension->SymbolicLink, &win32Name);
+		RtlAppendUnicodeStringToString(&pDeviceExtension->SymbolicLink, &pDeviceExtension->DiskRegInfo.DriverLetter);
+
+		status = WdfDeviceCreateSymbolicLink(device, &pDeviceExtension->SymbolicLink);
+	}
+
+	return status;
+}
+
+VOID RamdiskQueryDiskRegParameters(IN PWSTR RegistryPath, IN PDISK_INFO DiskRegInfo)
+{
+	NTSTATUS status;
+	DISK_INFO defDiskRegInfo;
+	RTL_QUERY_REGISTRY_TABLE rtlQueryRegTbl[5 + 1];
+
+
+	PAGED_CODE();
+	ASSERT(RegistryPath != NULL);
+
+	// set default values
+	defDiskRegInfo.DiskSize = DEFAULT_DISK_SIZE;
+	defDiskRegInfo.RootDirEntries = DEFAULT_ROOT_DIR_ENTRIES;
+	defDiskRegInfo.SectorsPerCluster = DEFAULT_SECTORS_PER_CLUSTER;
+	RtlInitUnicodeString(&defDiskRegInfo.DriverLetter, DEFAULT_DRIVE_LETTER);
+
+	// setup the query table
+	RtlZeroMemory(rtlQueryRegTbl, sizeof(rtlQueryRegTbl));
+
+	rtlQueryRegTbl[0].Flags = RTL_QUERY_REGISTRY_SUBKEY;
+	rtlQueryRegTbl[0].Name = L"Parameters";
+	rtlQueryRegTbl[0].EntryContext = NULL;
+	rtlQueryRegTbl[0].DefaultType = (ULONG_PTR)NULL;
+	rtlQueryRegTbl[0].DefaultData = NULL;
+	rtlQueryRegTbl[0].DefaultLength = (ULONG_PTR)NULL;
+	// Disk paramters
+	rtlQueryRegTbl[1].Flags = RTL_QUERY_REGISTRY_DIRECT;
+	rtlQueryRegTbl[1].Name = L"DiskSize";
+	rtlQueryRegTbl[1].EntryContext = &DiskRegInfo->DiskSize;
+	rtlQueryRegTbl[1].DefaultType = REG_DWORD;
+	rtlQueryRegTbl[1].DefaultData = &defDiskRegInfo.DiskSize;
+	rtlQueryRegTbl[1].DefaultLength = sizeof(ULONG);
+
+	rtlQueryRegTbl[2].Flags = RTL_QUERY_REGISTRY_DIRECT;
+	rtlQueryRegTbl[2].Name = L"RootDirEntries";
+	rtlQueryRegTbl[2].EntryContext = &DiskRegInfo->RootDirEntries;
+	rtlQueryRegTbl[2].DefaultType = REG_DWORD;
+	rtlQueryRegTbl[2].DefaultData = &defDiskRegInfo.RootDirEntries;
+	rtlQueryRegTbl[2].DefaultLength = sizeof(ULONG);
+
+	rtlQueryRegTbl[3].Flags = RTL_QUERY_REGISTRY_DIRECT;
+	rtlQueryRegTbl[3].Name = L"SectorsPerCluster";
+	rtlQueryRegTbl[3].EntryContext = &DiskRegInfo->SectorsPerCluster;
+	rtlQueryRegTbl[3].DefaultType = REG_DWORD;
+	rtlQueryRegTbl[3].DefaultData = &defDiskRegInfo.SectorsPerCluster;
+	rtlQueryRegTbl[3].DefaultLength = sizeof(ULONG);
+
+	rtlQueryRegTbl[4].Flags = RTL_QUERY_REGISTRY_DIRECT;
+	rtlQueryRegTbl[4].Name = L"DriveLetter";
+	rtlQueryRegTbl[4].EntryContext = &DiskRegInfo->DriverLetter;
+	rtlQueryRegTbl[4].DefaultType = REG_SZ;
+	rtlQueryRegTbl[4].DefaultData = defDiskRegInfo.DriverLetter.Buffer;
+	rtlQueryRegTbl[4].DefaultLength = 0;
+
+	status = RtlQueryRegistryValues(RTL_REGISTRY_ABSOLUTE | RTL_REGISTRY_OPTIONAL, RegistryPath, rtlQueryRegTbl, NULL, NULL);
+	if (FALSE == NT_SUCCESS(status))
+	{
+		DiskRegInfo->DiskSize = defDiskRegInfo.DiskSize;
+		DiskRegInfo->RootDirEntries = defDiskRegInfo.RootDirEntries;
+		DiskRegInfo->SectorsPerCluster = defDiskRegInfo.SectorsPerCluster;
+		RtlCopyUnicodeString(&DiskRegInfo->DriverLetter, &defDiskRegInfo.DriverLetter);
+	}
+
+	// print queried data
+	KdPrint(("DiskSize          = 0x%lx\n", DiskRegInfo->DiskSize));
+	KdPrint(("RootDirEntries    = 0x%lx\n", DiskRegInfo->RootDirEntries));
+	KdPrint(("SectorsPerCluster = 0x%lx\n", DiskRegInfo->SectorsPerCluster));
+	KdPrint(("DriveLetter       = %wZ\n", &(DiskRegInfo->DriverLetter)));
+
+	return;
+}
+
+NTSTATUS RamdiskFormatDisk(IN PDEVICE_EXTENSION pDeviceExtension)
+{
+
 }
--- driver/PRamdisk/PRamdisk/pramdisk.h
+++ driver/PRamdisk/PRamdisk/pramdisk.h
@@ -4,25 +4,49 @@
 #include <ntdddisk.h>
 
 #define NT_DEVICE_NAME	L"\\Device\\PRamdisk"
+#define DOS_DEVICE_NAME	 L"\\DosDevices\\"
 
+#define DRIVE_LETTER_BUFFER_SIZE  10
+#define DOS_DEVNAME_BUFFER_SZIE  (sizeof(DRIVE_LETTER_BUFFER_SIZE) + 10)
+
+#define RAMDISK_TAG 'DmaR'
+
+// default disk property
+#define DEFAULT_DISK_SIZE               (1024*1024)     // 1 MB
+#define DEFAULT_ROOT_DIR_ENTRIES        512
+#define DEFAULT_SECTORS_PER_CLUSTER     2
+#define DEFAULT_DRIVE_LETTER            L"Z:"
 
 typedef struct _DISK_INFO
 {
 	ULONG DiskSize;
+	ULONG RootDirEntries;
+	ULONG SectorsPerCluster;
+	UNICODE_STRING DriverLetter;
 } DISK_INFO, *PDISK_INFO;
 
 typedef struct _DEVICE_EXTENSION
 {
 	PUCHAR DiskImage;
 	DISK_GEOMETRY DiskGeometry;
-	DISK_INFO DIskRegInfo;
+	DISK_INFO DiskRegInfo;
 	// 
 	UNICODE_STRING SymbolicLink;
+
+	WCHAR DriverLetterBuffer[DRIVE_LETTER_BUFFER_SIZE];
+	WCHAR DosDeviceNameBuffer[DOS_DEVNAME_BUFFER_SZIE];
 	
 } DEVICE_EXTENSION, *PDEVICE_EXTENSION;
 
 // creates an accessor method with a specified name to access the device-extension
 WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(DEVICE_EXTENSION, DeviceGetExtension);
+
+typedef struct _QUEUE_EXTENSION
+{
+	PDEVICE_EXTENSION DeviceExtension;
+} QUEUE_EXTENSION, *PQUEUE_EXTENSION;
+
+WDF_DECLARE_CONTEXT_TYPE_WITH_NAME(QUEUE_EXTENSION, QueueGetExtension);
 
 
 DRIVER_INITIALIZE DriverEntry;
@@ -30,5 +54,17 @@ DRIVER_INITIALIZE DriverEntry;
 EVT_WDF_DRIVER_DEVICE_ADD RamdiskEvtDeviceAdd;
 
 EVT_WDF_DEVICE_CONTEXT_CLEANUP RamdiskEvtDeviceContextCleanup;
+
+EVT_WDF_IO_QUEUE_IO_READ RamdiskEvtIoRead;
+
+EVT_WDF_IO_QUEUE_IO_WRITE RamdiskEvtIoWrite;
+
+EVT_WDF_IO_QUEUE_IO_DEVICE_CONTROL RamdiskEvtIoDeviceControl;
+
+
+VOID RamdiskQueryDiskRegParameters(IN PWSTR RegistryPath, IN PDISK_INFO DiskRegInfo);
+
+NTSTATUS RamdiskFormatDisk(IN PDEVICE_EXTENSION deviceExtension);
+
 
 #endif
