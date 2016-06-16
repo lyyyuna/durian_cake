adc3b72466d5ebc1fb0e778d8e73ef7b6cea7409
lyyyuna <lyyyuna@gmail.com>
2016-06-16 22:35:01 +0800

    custom start io

---
+++ driver/windows_driver_development_internals/chap09/SpecialStartIo/SpecialStartIo/Source.cpp
@@ -0,0 +1,252 @@
+#include "Header.h"
+
+#pragma LOCKEDCODE
+VOID
+MyStartIo(
+IN PDEVICE_OBJECT pDeviceObject,
+IN PIRP pFirstIrp
+)
+{
+	KdPrint(("enter my start io\n"));
+
+	PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)pDeviceObject->DeviceExtension;
+	UNREFERENCED_PARAMETER(pDevExt);
+
+	PKDEVICE_QUEUE_ENTRY device_entry;
+
+	PIRP pIrp = pFirstIrp;
+
+	do
+	{
+		KEVENT event;
+		KeInitializeEvent(&event, NotificationEvent, FALSE);
+
+		LARGE_INTEGER timeout;
+		timeout.QuadPart = -3 * 1000 * 1000 * 10;
+
+		KeWaitForSingleObject(&event, Executive, KernelMode, FALSE, &timeout);
+
+		KdPrint(("Complete a irp:%x\n", pIrp));
+		pIrp->IoStatus.Status = STATUS_SUCCESS;
+		pIrp->IoStatus.Information = 0;	// no bytes xfered
+		IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+		device_entry = KeRemoveDeviceQueue(&pDevExt->device_queue);
+
+		KdPrint(("device entry:%x\n", device_entry));
+		if (device_entry == NULL)
+		{
+			break;
+		}
+
+		pIrp = CONTAINING_RECORD(device_entry, IRP, Tail.Overlay.DeviceQueueEntry);
+	} while (TRUE);
+
+	KdPrint(("leave My start io\n"));
+}
+
+
+
+#pragma INITCODE
+extern "C" NTSTATUS DriverEntry(
+	IN PDRIVER_OBJECT pDriverObject,
+	IN PUNICODE_STRING pRegistryPath
+	)
+{
+	UNREFERENCED_PARAMETER(pRegistryPath);
+
+	NTSTATUS status;
+	KdPrint(("enter driver entry\n"));
+
+	pDriverObject->DriverUnload = HelloDDKUnload;
+
+	pDriverObject->MajorFunction[IRP_MJ_CREATE] = HelloDDKDispatchRoutin;
+	pDriverObject->MajorFunction[IRP_MJ_CLOSE] = HelloDDKDispatchRoutin;
+	pDriverObject->MajorFunction[IRP_MJ_WRITE] = HelloDDKDispatchRoutin;
+	pDriverObject->MajorFunction[IRP_MJ_READ] = HelloDDKRead;
+	pDriverObject->MajorFunction[IRP_MJ_CLEANUP] = HelloDDKDispatchRoutin;
+	pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = HelloDDKDispatchRoutin;
+	pDriverObject->MajorFunction[IRP_MJ_SET_INFORMATION] = HelloDDKDispatchRoutin;
+	pDriverObject->MajorFunction[IRP_MJ_SHUTDOWN] = HelloDDKDispatchRoutin;
+	pDriverObject->MajorFunction[IRP_MJ_SYSTEM_CONTROL] = HelloDDKDispatchRoutin;
+
+	status = CreateDevice(pDriverObject);
+
+	KdPrint(("leave driverentry \n"));
+
+	return status;
+}
+
+#pragma PAGEDCODE
+NTSTATUS CreateDevice(
+	IN PDRIVER_OBJECT pDriverObject
+	)
+{
+	NTSTATUS status;
+	PDEVICE_OBJECT pDevObj;
+	PDEVICE_EXTENSION pDevExt;
+
+	UNICODE_STRING devName;
+	RtlInitUnicodeString(&devName, L"\\Device\\MyDDKDevice");
+
+	status = IoCreateDevice(pDriverObject,
+		sizeof(DEVICE_EXTENSION),
+		&devName,
+		FILE_DEVICE_UNKNOWN,
+		0, 
+		TRUE,
+		&pDevObj);
+
+	if (!NT_SUCCESS(status))
+		return status;
+
+	pDevObj->Flags |= DO_BUFFERED_IO;
+	pDevExt = (PDEVICE_EXTENSION)pDevObj->DeviceExtension;
+	pDevExt->pDevice = pDevObj;
+	pDevExt->ustrDeviceName = devName;
+
+	RtlZeroBytes(&pDevExt->device_queue, sizeof(pDevExt->device_queue));
+	KeInitializeDeviceQueue(&pDevExt->device_queue);
+
+	UNICODE_STRING symLinkName;
+	RtlInitUnicodeString(&symLinkName, L"\\??\\HelloDDK");
+	pDevExt->ustrSymLinkName = symLinkName;
+	status = IoCreateSymbolicLink(&symLinkName, &devName);
+
+	if (!NT_SUCCESS(status))
+	{
+		IoDeleteDevice(pDevObj);
+		return status;
+	}
+
+	return STATUS_SUCCESS;
+}
+
+#pragma PAGEDCODE
+VOID HelloDDKUnload(IN PDRIVER_OBJECT pDriverObject)
+{
+	PDEVICE_OBJECT	pNextObj;
+	KdPrint(("Enter DriverUnload\n"));
+	pNextObj = pDriverObject->DeviceObject;
+	while (pNextObj != NULL)
+	{
+		PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)
+			pNextObj->DeviceExtension;
+
+		UNICODE_STRING pLinkName = pDevExt->ustrSymLinkName;
+		IoDeleteSymbolicLink(&pLinkName);
+
+		pNextObj = pNextObj->NextDevice;
+		IoDeleteDevice(pDevExt->pDevice);
+	}
+}
+
+#pragma PAGEDCODE
+NTSTATUS HelloDDKDispatchRoutin(IN PDEVICE_OBJECT pDevObj,
+	IN PIRP pIrp)
+{
+	UNREFERENCED_PARAMETER(pDevObj);
+
+	KdPrint(("Enter HelloDDKDispatchRoutin\n"));
+
+	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(pIrp);
+
+	static char* irpname[] =
+	{
+		"IRP_MJ_CREATE",
+		"IRP_MJ_CREATE_NAMED_PIPE",
+		"IRP_MJ_CLOSE",
+		"IRP_MJ_READ",
+		"IRP_MJ_WRITE",
+		"IRP_MJ_QUERY_INFORMATION",
+		"IRP_MJ_SET_INFORMATION",
+		"IRP_MJ_QUERY_EA",
+		"IRP_MJ_SET_EA",
+		"IRP_MJ_FLUSH_BUFFERS",
+		"IRP_MJ_QUERY_VOLUME_INFORMATION",
+		"IRP_MJ_SET_VOLUME_INFORMATION",
+		"IRP_MJ_DIRECTORY_CONTROL",
+		"IRP_MJ_FILE_SYSTEM_CONTROL",
+		"IRP_MJ_DEVICE_CONTROL",
+		"IRP_MJ_INTERNAL_DEVICE_CONTROL",
+		"IRP_MJ_SHUTDOWN",
+		"IRP_MJ_LOCK_CONTROL",
+		"IRP_MJ_CLEANUP",
+		"IRP_MJ_CREATE_MAILSLOT",
+		"IRP_MJ_QUERY_SECURITY",
+		"IRP_MJ_SET_SECURITY",
+		"IRP_MJ_POWER",
+		"IRP_MJ_SYSTEM_CONTROL",
+		"IRP_MJ_DEVICE_CHANGE",
+		"IRP_MJ_QUERY_QUOTA",
+		"IRP_MJ_SET_QUOTA",
+		"IRP_MJ_PNP",
+	};
+
+	UCHAR type = stack->MajorFunction;
+	if (type >= arraysize(irpname))
+		KdPrint((" - Unknown IRP, major type %X\n", type));
+	else
+		KdPrint(("\t%s\n", irpname[type]));
+
+
+	NTSTATUS status = STATUS_SUCCESS;
+
+	pIrp->IoStatus.Status = status;
+	pIrp->IoStatus.Information = 0;	// bytes xfered
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+	KdPrint(("Leave HelloDDKDispatchRoutin\n"));
+
+	return status;
+}
+
+
+VOID OnCancelIRP(
+	IN PDEVICE_OBJECT pDeviceObject,
+	IN PIRP pIrp
+	)
+{
+	UNREFERENCED_PARAMETER(pDeviceObject);
+
+	KdPrint(("enter cancel read irp\n"));
+
+	IoReleaseCancelSpinLock(pIrp->CancelIrql);
+
+	pIrp->IoStatus.Status = STATUS_CANCELLED;
+	pIrp->IoStatus.Information = 0;
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+
+
+	KdPrint(("leave cancel read irp\n"));
+}
+
+
+NTSTATUS HelloDDKRead(
+	IN PDEVICE_OBJECT pDevObj,
+	IN PIRP pIrp
+	)
+{
+	KdPrint(("enter hello ddk read\n"));
+
+	PDEVICE_EXTENSION pDevExt = (PDEVICE_EXTENSION)pDevObj->DeviceExtension;
+
+	IoMarkIrpPending(pIrp);
+
+	IoSetCancelRoutine(pIrp, OnCancelIRP);
+
+	KIRQL oldirql;
+	KeRaiseIrql(DISPATCH_LEVEL, &oldirql);
+	KdPrint(("helloddk read irp: %x\n", pIrp));
+
+	KdPrint(("DeviceQueueEntry:%x\n", &pIrp->Tail.Overlay.DeviceQueueEntry));
+
+	if (!KeInsertDeviceQueue(&pDevExt->device_queue, &pIrp->Tail.Overlay.DeviceQueueEntry))
+		MyStartIo(pDevObj, pIrp);
+
+	KeLowerIrql(oldirql);
+
+	KdPrint(("leave hello ddk read\n"));
+
+	return STATUS_SUCCESS;
+}
