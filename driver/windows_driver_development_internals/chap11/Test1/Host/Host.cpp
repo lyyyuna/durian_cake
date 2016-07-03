6d823ba4f5cd24494f5c9a293f3a15208e7638db
lyyyuna <lyyyuna@gmail.com>
2016-07-03 15:31:59 +0800

    console

---
+++ driver/windows_driver_development_internals/chap11/Test1/Host/Host.cpp
@@ -0,0 +1,34 @@
+// Host.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <Windows.h>
+#include <stdio.h>
+
+
+int main()
+{
+	HANDLE hDevice =
+		CreateFile(L"\\\\.\\HelloDDKB",
+		GENERIC_READ | GENERIC_WRITE,
+		0,		// share mode none
+		NULL,	// no security
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);		// no template
+
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		printf("Failed to obtain file handle to device "
+			"with Win32 error code: %d\n",
+			GetLastError());
+		return 1;
+	}
+
+	DWORD dRet;
+	ReadFile(hDevice, NULL, 0, &dRet, NULL);
+
+	CloseHandle(hDevice);
+
+	return 0;
+}
+
