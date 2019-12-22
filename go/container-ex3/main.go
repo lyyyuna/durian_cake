16c4ba8e24fe5e1307503a2639c0658a7ad69401
lyyyuna <lyyyuna@outlook.com>
2019-12-22 19:57:45 +0800

    net namespace

---
+++ go/container-ex3/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os/exec"
+	"log"
+	"os"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
+		syscall.CLONE_NEWNET}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
