5d6afdaff4794ea52e8772a48c61de447e2096f3
lyyyuna <lyyyuna@outlook.com>
2019-12-22 20:36:03 +0800

    user namespace

---
+++ go/container-ex4/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os/exec"
+	"log"
+	"os"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{Cloneflags: syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
+		syscall.CLONE_NEWNET | syscall.CLONE_NEWUSER,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID:	0,
+				Size:   1,
+			},	
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 0,
+				HostID: 0,
+				Size:   1,
+			},
+		},
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
