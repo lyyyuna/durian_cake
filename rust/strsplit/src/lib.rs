51892cd604f5fb7459d5f442023af0155c57a78b
lyyyuna <lyyyuna@outlook.com>
2020-06-04 20:34:42 +0800

    update

--- rust/strsplit/src/lib.rs
+++ rust/strsplit/src/lib.rs
@@ -2,14 +2,14 @@
 
 #[derive(Debug)]
 pub struct StrSplit<'a> {
-    remainder: &'a str,
+    remainder: Option<&'a str>,
     delimiter: &'a str,
 }
 
 impl<'a> StrSplit<'a> {
     pub fn new(haystack: &'a str, delimiter: &'a str) -> Self {
         Self {
-            remainder: haystack,
+            remainder: Some(haystack),
             delimiter,
         }
     }
@@ -18,17 +18,13 @@ impl<'a> StrSplit<'a> {
 impl<'a> Iterator for StrSplit<'a> {
     type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(next_delim) = self.remainder.find(self.delimiter) {
-            let until_delim = &self.remainder[..next_delim];
-            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
-            Some(until_delim)
-        } else if self.remainder.is_empty() {
-            None
+        let ref mut remainder = self.remainder?;
+        if let Some(next_delim) = remainder.find(self.delimiter) {
+            let until_delimiter = &remainder[..next_delim];
+            *remainder = &remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
         } else {
-            let rest = self.remainder;
-            self.remainder = "";
-            // &'a str = &'static str ?
-            Some(rest)
+            self.remainder.take()
         }
     }
 }
@@ -38,4 +34,11 @@ fn it_works() {
     let haystack = "a b c d e f";
     let letters =  StrSplit::new(haystack, " ");
     assert!(letters.eq(vec!["a", "b", "c", "d", "e", "f"].into_iter()));
+}
+
+#[test]
+fn tail() {
+    let haystack = "a b c d e ";
+    let letters =  StrSplit::new(haystack, " ");
+    assert!(letters.eq(vec!["a", "b", "c", "d", "e", ""].into_iter()));
 }
