28efbc9747764d46fa7610c67ab1fcfabfa42275
lyyyuna <lyyyuna@outlook.com>
2020-06-24 15:46:54 +0800

    update

---
+++ rust/vecmac/src/lib.rs
@@ -0,0 +1,34 @@
+#[macro_export]
+macro_rules! avec {
+    () => {
+        Vec::new()
+    };
+    ($($element:expr),+) => {{
+        let mut vs = Vec::new();
+        $(vs.push($element);)*
+        vs
+    }}
+}
+
+#[test]
+fn empty_vec() {
+    let x: Vec<u32> = avec![];
+    assert!(x.is_empty());
+}
+
+#[test]
+fn single() {
+    let x: Vec<u32> = avec![42];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 1);
+    assert_eq!(x[0], 42);
+}
+
+#[test]
+fn double() {
+    let x: Vec<u32> = avec![42, 34];
+    assert!(!x.is_empty());
+    assert_eq!(x.len(), 2);
+    assert_eq!(x[0], 42);
+    assert_eq!(x[1], 34);
+}
