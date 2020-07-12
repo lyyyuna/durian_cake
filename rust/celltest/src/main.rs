42a7ba413e6870600aced367a15590d5ed498b5e
lyyyuna <lyyyuna@gmail.com>
2020-07-12 16:26:33 +0800

    update

---
+++ rust/celltest/src/main.rs
@@ -0,0 +1,20 @@
+use std::fmt;
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+
+fn main() {
+    let data = Point{x:0, y:1};
+    let p = data;
+    //data = 10;
+    let c = data;
+  }
