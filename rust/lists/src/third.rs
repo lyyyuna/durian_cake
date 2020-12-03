849247f86992891c167ae889ee2614382fe23abe
lyyyuna <lyyyuna@outlook.com>
2020-12-03 20:32:51 +0800

    iterator for stack

--- rust/lists/src/third.rs
+++ rust/lists/src/third.rs
@@ -44,6 +44,33 @@ impl<T> List<T> {
     }
 }
 
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_ref().map(|node| {
+                & **node
+            })
+        }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| {
+                & **node
+            });
+            &node.elem
+        })
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
